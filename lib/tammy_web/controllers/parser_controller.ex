a3ae37439c038c07ab9b079c8498ca1f77fa6ed6
mkreyman <mark@kreyman.com>
2018-06-25 19:45:05 -0600

    Removed extra logging

--- lib/tammy_web/controllers/parser_controller.ex
+++ lib/tammy_web/controllers/parser_controller.ex
@@ -7,13 +7,10 @@ defmodule TammyWeb.ParserController do
 
   def handle_call(conn, params) do
     with {:match, updated_params} <- Filter.match_recipient(params) do
-      email =
-        updated_params
-        |> Parser.normalize()
-        |> Email.compose()
-
-      Mailer.deliver_later(email)
-      Logger.info("Queued for delivery: \n#{inspect(email)}")
+      updated_params
+      |> Parser.normalize()
+      |> Email.compose()
+      |> Mailer.deliver_later()
     else
       {:no_match, to, map} ->
         Logger.info(
